209ac7799e44bdb6cd9c7c9a95267638f7585973
ha-gi <hediye.aktas@sjsu.edu>
2023-07-21 13:32:01 -0700

    arduino code now understands direction

--- code-on-arduino/take-serial-input.ino
+++ code-on-arduino/take-serial-input.ino
@@ -1,22 +1,34 @@
 /*
-On the serial port the input data will come in the format 10,2,3,4 basically 4 comma seperated values.
+On the serial port the input data will come in the format 10,-2,3,4 basically 4 comma seperated values.
 
 This code will seperate it into 4 values and then run a function on each value.
 
-The first value is # of screw turns for Motor A X Axis
-The second value is # of screw turns for Motor A Y Axis
+The first value i.e. 10 is # of screw turns for Motor A X Axis in the clockwise direction.
+The second value i.e. -2 indicates 2 is # of screw turns for Motor A Y Axis in the anti clockwise direction.
 
-The third value is # of screw turns for Motor B X Axis
-The fourth value is # of screw turns for Motor B Y Axis
+The third value i.e. 3 indicates 3 is # of screw turns for Motor B X Axis in the clockwise direction.
+The fourth value i.e. 4 indicates 4 is # of screw turns for Motor B Y Axis in the anti clockwise direction.
 
 So there are 4 kinds of screw turns. 2 for Motor A (X and Y Axis) and 2 for Motor B (X and Y axis)
+
+
+
 */
 
 // Motor control pins
-int motorA_step_pin = 11; // B step pin
-int motorA_dir_pin = 10; // B direction pin
-int motorB_step_pin = 9; // C step pin
-int motorB_dir_pin = 8; // C direction pin
+// This is motor A that will control the first mirror
+int motorA_X_step_pin = 11; // A step pin
+int motorA_X_dir_pin = 10; // A direction pin
+
+int motorA_Y_step_pin = 9; // A step pin
+int motorA_Y_dir_pin = 8; // A direction pin
+
+// This is motor B that will control the second mirror To disable set the pin numbers to 0
+int motorB_X_step_pin = 0; // B step pin
+int motorB_X_dir_pin = 0; // B direction pin
+
+int motorB_Y_step_pin = 0; // B step pin
+int motorB_Y_dir_pin = 0; // B direction pin
 
 void setup()
 {
@@ -33,8 +45,22 @@ void setup()
   pinMode(motorB_dir_pin, OUTPUT); 
 }
 
-void do_steps(int dir, int turns, int step_pin, int dir_pin) {
-  digitalWrite(dir_pin, dir);
+void do_steps(int turns, int step_pin, int dir_pin) {
+  
+  // If the step pin or direction pin is not set then return
+  // Allow the user to disable a motor by setting the pin numbers to 0 
+  if(step_pin ==0 || dir_pin == 0) {
+    return;
+  }
+
+  // Set the direction pin. to go clockwise set dir = 1, to go anti clockwise set dir = 0
+  if(turns > 0)
+    digitalWrite(dir_pin, 1);
+  else if(turns < 0)
+    digitalWrite(dir_pin, 0);
+  else if(turns == 0)
+    return;
+  
   for(int n = 0; n < turns; n++) {
     digitalWrite(step_pin, 1);
     delay(1);
@@ -67,9 +93,9 @@ void loop()
     int motorB_Y_turns = incomingData[3];
 
     // Execute steps based on the number of turns for each Motor and axis
-    do_steps(1, motorA_X_turns, motorA_step_pin, motorA_dir_pin);
-    do_steps(1, motorA_Y_turns, motorB_step_pin, motorB_dir_pin);
-    do_steps(1, motorB_X_turns, motorA_step_pin, motorA_dir_pin);
-    do_steps(1, motorB_Y_turns, motorB_step_pin, motorB_dir_pin);
+    do_steps(motorA_X_turns, motorA_step_pin, motorA_dir_pin);
+    do_steps(motorA_Y_turns, motorA_step_pin, motorA_dir_pin);
+    do_steps(motorB_X_turns, motorB_step_pin, motorB_dir_pin);
+    do_steps(motorB_Y_turns, motorB_step_pin, motorB_dir_pin);
   }
 }
